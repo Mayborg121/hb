6acb536579fc5a633df76d34cade6eb7da2c0e40
Mayur borgude <72288591+Mayborg121@users.noreply.github.com>
2025-01-07 15:11:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+const generated_link = document.getElementById('generated_link');
+const fName = document.getElementById('fName');
+const mName = document.getElementById('mName');
+let f_link = "https://mayborg121.github.io/hb/h.html/" ;
+let link_copy = document.getElementById('link_copy');
+
+function gen_link(){
+    event.preventDefault();
+
+    const birthDate = document.getElementById('birthDate').value;
+    const bdate = new Date(birthDate);
+
+    // Extract day and month
+    const day = bdate.getDate();
+    const month = bdate.getMonth() + 1; // Month is 0-based, so add 1
+
+    // Format as "day.month"
+    const formattedDate = String(`${day}.${month}`);
+
+    console.log(day);
+
+
+
+    if(Number.isInteger(day)){
+
+
+
+    const gen_link = String("https://mayborg121.github.io/hb/h.html/"+"?"+"n="+fName.value+"&"+"m="+mName.value+"&"+"d="+formattedDate) ;
+
+    
+    
+    console.log(gen_link);
+    link_copy.innerHTML = ` <div id="link_container"> <p id="generated_link">${gen_link}</p> </div>
+                            <button id="copyButton">Copy</button>
+                            <button id="visit_link">Visit</button>`;
+
+
+    f_link = gen_link;
+
+    }
+
+    else{
+        link_copy.innerHTML = ` <div id="link_container"> <p id="generated_link"> "Please Select Date" </p> </div>
+                            <button id="copyButton">Copy</button>
+                            <button id="visit_link">Visit</button>`;
+    }
+
+
+    document.getElementById("copyButton").addEventListener("click", function () {
+        copyButton.innerText = "Copied !";
+        // Use Clipboard API to copy the text
+        navigator.clipboard.writeText(f_link);
+          
+    });
+    
+    document.getElementById("visit_link").addEventListener("click", function () {
+        const link = f_link; // Get the text
+      
+        // Open the link in a new tab
+        window.open(link, "_blank");
+    });
+}
+
+
